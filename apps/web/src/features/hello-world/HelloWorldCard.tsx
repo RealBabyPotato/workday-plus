0bcaa165e73f488a4f220642cdd35958d90cc5e5
Andre Xue <150495620+pickleisgood@users.noreply.github.com>
2026-03-20 15:00:22 -0700

    new api button (#3)
    lgtm

--- apps/web/src/features/hello-world/HelloWorldCard.tsx
+++ apps/web/src/features/hello-world/HelloWorldCard.tsx
@@ -1,6 +1,7 @@
 import { useQuery, type UseQueryResult } from '@tanstack/react-query'
 
 import { fetchHello } from '../../api/hello'
+import { fetchTest } from '../../api/test'
 import { SectionCard } from '../../components/SectionCard'
 import { useState } from 'react';
 
@@ -8,6 +9,13 @@ export function HelloWorldCard() {
   const helloQuery = useQuery({
     queryKey: ['hello'],
     queryFn: fetchHello,
+  })
+
+  const [exampleId, setExampleId] = useState<number>(1);
+
+  const andreQuery = useQuery({
+    queryKey: ['test', exampleId],
+    queryFn: () => fetchTest(exampleId),
   })
 
   const [num, setNum] = useState(0);
@@ -46,6 +54,19 @@ export function HelloWorldCard() {
             <p className="mt-1">{helloQuery.data.frontend_hint}</p>
           </div>
         ) : null}
+        
+        <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700" onClick={() => {
+            setExampleId(exampleId + 1);
+        }}>next one</button>
+
+        {andreQuery.data ? (
+          <div className="rounded-lg bg-sky-50 p-4 text-sky-1500">
+            <p className="font-large">{andreQuery.data.id}</p>
+            <p className="mt-2">title: {andreQuery.data.title}</p>
+            <p>here!!</p>
+            
+          </div>
+        ) : null }
       </div>
     </SectionCard>
   )
