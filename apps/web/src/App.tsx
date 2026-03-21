b9f5ce81754a99b78469b6bab1a0dd03a93d1162
Ben Zhou <bzhou4@student.ubc.ca>
2026-03-20 22:36:14 -0700

    page setup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
 import { HomePage } from './pages/HomePage'
+import ProgramSelection from './pages/ProgramSelection'
 
 function App() {
-  return <HomePage />
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/programselection" element={<ProgramSelection />} />
+    </Routes>
+  )
 }
 
 export default App
